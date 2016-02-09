3f8c834fd63e24176b65573e4b24dc15a078ad0c
Damir Brekalo <damir.brekalo@trikoder.net>
2016-02-09 14:04:09 +0100

    Added grunt bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -102,6 +102,15 @@ module.exports = function(grunt) {
                     spawn: false
                 }
             }
+        },
+
+        bump: {
+            options: {
+                files: ['package.json', 'bower.json'],
+                commitFiles: ['package.json', 'bower.json'],
+                tagName: '%VERSION%',
+                push: false
+            }
         }
 
     });
